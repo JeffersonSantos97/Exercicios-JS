5ada1c4bcdfa2bc9257d9d0ea70aeb40f244daa6
Mandume97 <jeffersonbelo3494@gmail.com>
2021-11-26 15:52:29 -0300

    atualização ex001 commit

--- 1 - INTRODUÇÃO JS/ex001.js
+++ 1 - INTRODUÇÃO JS/ex001.js
@@ -1,29 +1,30 @@
 /* 01. Escreva um programa que pede o raio de um círculo, 
 e em seguida exiba o perímetro e área do círculo.
 */
-const r = prompt("Digite o raio de um circulo: ");
+let raio = document.querySelector("#raio");
+ //Raio com o valor inteiro 
+let rPerimetro = document.querySelector("#perimetro");
+let rArea = document.querySelector("#area");
 
 //Usei o prompt pra abrir a uma janela no navegador
 
-const a = area(r).toFixed(1);
+
 
 //Crei uma constante ( a ) para receber a função
 //To fixed() é pra definir quantas casas decimais após a virgula 
 
-console.log(a);
-
-const p = perimetro(r).toFixed(1);
-// Mesma coisa que eu fiz com o ( a )
-console.log(p);
-
-
-function area(r){
-    return Math.PI * (r*r);
-    //Math.PI é um objeto que retorna ( PI )
+function area(){
+    let r = parseInt(raio.value);
+    rArea.innerHTML =  (Math.PI * (r*r)).toFixed(2);
 }
 //A função retorna a formula da area do circulo
-function perimetro(r){
-    return 2 * Math.PI * r;
-    }
+function perimetro(){ 
+    let r = parseInt(raio.value);
+    rPerimetro.innerHTML = (2 * Math.PI * r).toFixed(2);
+}
 
 // A função retorna a formula do perímetro do circulo
+function pAndA(){
+    area();
+    perimetro();
+}
