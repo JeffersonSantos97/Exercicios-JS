414c0b25c3797924ffc8b21a67a6d4948f53b24c
Mandume97 <jeffersonbelo3494@gmail.com>
2021-11-30 13:31:55 -0300

    add DOM commit

--- 2 - CONDICIONAL/ex004.js
+++ 2 - CONDICIONAL/ex004.js
@@ -1,22 +1,25 @@
 /*4. Faça um script que verifique se uma letra 
 digitada num campo de input é vogal ou consoante.*/
 
-const info = (prompt("Digite uma letra:")).toLowerCase();
+let info = document.querySelector("#letra");
+let buttonVerificar = document.querySelector("#buttonVerificar");
+let resposta = (document.querySelector("span"));
+
 //Esse .toLowerCase() serve pra deixar todas as letras que forem digitadas em minusculo
 
 //Precisa de letra minuscula pq no JAVASCRIPT (a) é diferente de (A)
 //Então só deixa tudo minusculo, pq faz um tipo só de comparação
-function VogConso(v){
-    
-    if (v === "a" || v === "e" || v === "i" || v === "o" || v === "u"){
-        alert(`A letra ${v} é uma vogal`)
+
+function VogConso(){
+    let i = (info.value).toLowerCase();
+    if (i === "a" || i === "e" || i === "i" || i === "o" || i === "u"){
+        resposta.innerHTML = "VOGAL"
     }
 //Se qualquer dessa letras estiver, vai retornar vogal, mas se não estiver, vai aparecer consoante
     else {
-        alert(`A letra ${v} é uma consoante`)
+        resposta.innerHTML = "CONSOANTE"
     };
-    
-
 }
-VogConso(info);
+buttonVerificar.addEventListener("click",VogConso);
+
 //Chamando a função
--- 2 - CONDICIONAL/ex005.js
+++ 2 - CONDICIONAL/ex005.js
@@ -4,36 +4,28 @@
     A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
     A mensagem "Reprovado", se a média for menor do que sete;
     A mensagem "Aprovado com Distinção", se a média for igual a dez.
-    */ 
+   
+    */
 
-const n1 = parseFloat(prompt("digite a nota 1: "));
-//Peddindo a primeira nota 
-
-//Obs: Se não usar o parseFloat ou o parseInt, a nota que o usuário colocar 
-//vai vir como uma string e isso vai fazer dar um erro no seu cálculo 
-const n2 = parseFloat(prompt("digite a nota 2: "));
-//Pedidndo a segunda nota 
-
-const m = media(n1,n2);
-//crei uma constante pra jogar a função media 
-
-resultado();
-//Função resultado autoexecutável 
-function media(n1,n2){
-  
-    return ((n1 + n2) / 2).toFixed(1);
-    //Como eu quero que sempre apareça a meédia com pelo menos um numero após a virgula, eu coloquei o (toFixed) 
     
-}
+    let buttoncalc = document.getElementById("calc");
+    let m = document.getElementById("media");
+    let result = document.getElementById("result");
 
-function resultado(){
-    if(m >= 10 ){
-        return alert(`Sua média foi ${m} e vocÊ foi APROVADO COM DISTINÇÃO`)
+    function media(){
+        let nota1 = parseFloat(document.getElementById("n1").value);
+    let nota2 = parseFloat(document.getElementById("n2").value);
+        m.innerHTML = (nota1 + nota2) / 2;
+        var med =  (nota1 + nota2) / 2;
+        
+        if(med === 10){
+            result.innerHTML = "aprovado com distinção"
+        }
+        else if(med >=7){
+            result.innerHTML = "aprovado"
+        }
+        else{
+            result.innerHTML = "REPROVADO"
+        }
     }
-    else if ( m >= 7){
-        return alert(`Sua média foi ${m} e você está APROVADO`)
-    }
-    else {
-        alert(`Sua média foi ${m} e você está REPROVADO`)
-    }
-}
+    buttoncalc.addEventListener("click",media);
